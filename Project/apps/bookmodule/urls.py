85da93e746cb284c0b1a0659d7c4ea0c562c5ff0
ifaris25 <farisal7mad@gmail.com>
2024-10-11 17:48:06 +0300

    Lab5

--- Project/apps/bookmodule/urls.py
+++ Project/apps/bookmodule/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path , include
 
 urlpatterns = [
-    path('',views.index),
-    path('index2/<int:val1>/', views.index2),
-    path('<int:bookId>', views.viewbook),
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('viewbook/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
